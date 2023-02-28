7a80c05e33150f771ea0b8a411e2ba62010c46ac
Vitor Lobo Ramos <lobocode@gmail.com>
2023-02-28 18:48:26 -0300

    fix output

--- infra/modules/nodes/output.tf
+++ infra/modules/nodes/output.tf
@@ -5,11 +5,3 @@ output "eks_node_group" {
 output "eks_node_role" {
   value = aws_iam_role.eks_nodes_roles
 }
-
-output "eks_node_sg" {
-  value = aws_security_group.eks_node_group
-}
-
-output "eks_node_sg_id" {
-  value = aws_security_group.eks_node_group.id
-}
